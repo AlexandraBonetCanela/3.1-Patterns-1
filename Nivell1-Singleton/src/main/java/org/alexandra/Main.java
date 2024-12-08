1245d4fa4ab969d590b22764e3a6e16da1e74ee0
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2024-12-08 12:20:11 +0100

    switched if else to swtich case and moved to an external method

--- Nivell1-Singleton/src/main/java/org/alexandra/Main.java
+++ Nivell1-Singleton/src/main/java/org/alexandra/Main.java
@@ -3,31 +3,38 @@ package org.alexandra;
 import java.util.Scanner;
 
 public class Main {
+    private static boolean finish;
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        boolean finish = false;
+        finish = false;
         System.out.println("Welcome to AddYourCommandsApplication!");
         System.out.println("Add as many commands as you want, to undo last command type 'undo', to see last commands type 'history' and to finish type 'finish'");
         Undo undo = Undo.getInstance();
 
         while (!finish){
-            String command = scanner.nextLine();
-            if (command.equalsIgnoreCase("FINISH")){
-                finish = true;
-            }
-            else if (command.equalsIgnoreCase("UNDO")){
-                undo.removeLastCommand();
-                System.out.println("command undone");
-            }
-            else if (command.equalsIgnoreCase("HISTORY")){
-                System.out.println("Last commands:");
-                undo.showHistory();
-            }
-            else {
-                undo.addCommand(command);
-            }
+            String command = scanner.nextLine().toUpperCase();
+            processCommand(command);
         }
         System.out.println("Thank you for spending time with us today. See you next time!");
+    }
+
+    public static void processCommand(String command){
+        switch (command){
+            case "FINISH":
+                finish = true;
+                break;
+            case "UNDO":
+                Undo.getInstance().removeLastCommand();
+                System.out.println("command undone");
+                break;
+            case "HISTORY":
+                System.out.println("Last commands:");
+                Undo.getInstance().showHistory();
+                break;
+            default:
+                Undo.getInstance().addCommand(command);
+        }
     }
 }
